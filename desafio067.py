9e89b4f8e3b2737a84310f75b5edf6bad4fcb5f8
Junior Borges <48998781+abbjunior@users.noreply.github.com>
2021-11-08 17:19:28 -0300

    Add files via upload

---
+++ desafio067.py
@@ -0,0 +1,12 @@
+# Faça um programa que mostre a tabuada de vários números, um de cada vez, 
+# para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for 
+# negativo.
+
+while True:
+    n = int(input("Digite o número que deseja ver a tabuada: "))
+    if n < 0:
+        print('FIM DO PROGRAMA !')
+        break
+    else:
+        for num in range (1,11):
+            print(f'{num} x {n} = {num*n}')
